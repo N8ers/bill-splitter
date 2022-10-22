c46895f7a78943a8082a569e94bccc293b8daff8
sheryak_n <nathanraysheryak@gmail.com>
2022-10-22 12:57:40 -0400

    build poc (all in App.tsx)

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,8 +4,6 @@ import App from "./App"
 
 test("renders header text", () => {
   render(<App />)
-  const headerText = screen.getByText(
-    /Type in the input for your message to appear!/i
-  )
+  const headerText = screen.getByText(/Split your bill/i)
   expect(headerText).toBeInTheDocument()
 })
--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,75 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 
-import { TextField } from "@mui/material"
+import { TextField, Divider } from "@mui/material"
 
 import "./App.css"
 
 function App() {
-  const [message, setMessage] = useState("")
+  const [total, setTotal] = useState("")
+
+  const [incomeOne, setIncomeOne] = useState("")
+  const [incomeTwo, setIncomeTwo] = useState("")
+
+  const [incomePercentOne, setIncomePercentOne] = useState(0)
+  const [incomePercentTwo, setIncomePercentTwo] = useState(0)
+
+  useEffect(() => {
+    const totalIncome = Number(incomeOne) + Number(incomeTwo)
+    const personOnePercent = parseInt(incomeOne) / Number(totalIncome)
+    const personTwoPercent = 1 - personOnePercent
+
+    setIncomePercentOne(personOnePercent)
+    setIncomePercentTwo(personTwoPercent)
+  }, [total, incomeOne, incomeTwo])
 
   return (
     <div className="App">
-      <h4>Type in the input for your message to appear!</h4>
+      <h4>Split your bill</h4>
 
-      <TextField
-        label="Message"
-        variant="standard"
-        onChange={(e) => setMessage(e.target.value)}
-      />
+      <div>
+        <h3>Total: {total}</h3>
+        <TextField
+          label="Total Bill"
+          variant="standard"
+          onChange={(e) => setTotal(e.target.value)}
+        />
+      </div>
 
-      <div>{message}</div>
+      <Divider />
+
+      <div>
+        <h3>Person 1 Income: {incomeOne}</h3>
+        <TextField
+          label="Person 1 Income"
+          variant="standard"
+          onChange={(e) => setIncomeOne(e.target.value)}
+        />
+        <div>
+          <div>Percent of income: {(incomePercentOne * 100).toFixed(1)}%</div>
+          <div>
+            Total to contribute: $
+            {(Number(total) * incomePercentOne).toFixed(2)}
+          </div>
+        </div>
+      </div>
+
+      <Divider />
+
+      <div>
+        <h3>Person 2 Income: {incomeTwo}</h3>
+        <TextField
+          label="Person 2 Income"
+          variant="standard"
+          onChange={(e) => setIncomeTwo(e.target.value)}
+        />
+        <div>
+          <div>Percent of income: {(incomePercentTwo * 100).toFixed(1)}%</div>
+          <div>
+            Total to contribute: $
+            {(Number(total) * incomePercentTwo).toFixed(2)}
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
