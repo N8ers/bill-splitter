471c6fda9d09097a39c73256b803ba50a46a28a6
sheryak_n <nathanraysheryak@gmail.com>
2022-10-22 08:15:37 -0400

    remove default content, add content, add workflow

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders silly text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const sillyText = screen.getByText(/hi :P/i);
+  expect(sillyText).toBeInTheDocument();
 });
