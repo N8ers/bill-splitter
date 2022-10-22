a2cb1359a5c776ed63ecbaa94dc195d899c44d9a
sheryak_n <nathanraysheryak@gmail.com>
2022-10-22 08:45:25 -0400

    add mui + make input binding

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import {useState} from 'react';
+
+import {TextField} from '@mui/material';
+
 import './App.css';
 
 function App() {
+  const [message, setMessage] = useState('')
+
   return (
     <div className="App">
-      hi :P
+      <h4>Type in the input for your message to appear!</h4>
+
+      <TextField 
+        label="Message" 
+        variant="standard" 
+        onChange={(e) => setMessage(e.target.value)} 
+      />
+
+      <div>{message}</div>
     </div>
   );
 }
