c3f3ea6a395f42f3f541cd2508dbedc80d996528
sheryak_n <nathanraysheryak@gmail.com>
2022-10-28 06:18:30 -0400

    select some colors + cool backgound/header

--- src/components/BasicGrid/BasicGrid.tsx
+++ src/components/BasicGrid/BasicGrid.tsx
@@ -1,11 +1,18 @@
 import * as React from "react"
 import Box from "@mui/material/Box"
 import Grid from "@mui/material/Unstable_Grid2"
-import { TextField } from "@mui/material"
+import { TextField, Button } from "@mui/material"
 
 import styles from "./BasicGrid.module.css"
 
 export default function BasicGrid() {
+  const Header = (
+    <div className={styles.header}>
+      <h1>Bill Splitter</h1>
+      <p>Split your expense by income.</p>
+    </div>
+  )
+
   const Total = (
     <div className={styles.total}>
       <div>Split Your Bill</div>
@@ -19,26 +26,33 @@ export default function BasicGrid() {
       <Grid xs={2}>
         <div>Income 1</div>
       </Grid>
-      <Grid xs={5}>
+      <Grid xs={4}>
         <div>Share: $0.00</div>
         <div>Percent: 0.0%</div>
       </Grid>
-      <Grid xs={5}>
+      <Grid xs={4}>
         <TextField label="income" variant="standard" />
+      </Grid>
+      <Grid xs={2}>
+        <Button>Expense!</Button>
       </Grid>
     </Grid>
   )
 
   return (
-    <Box sx={{ flexGrow: 1 }} className={styles.container}>
-      <Grid container spacing={2}>
-        <Grid xs={4}>{Total}</Grid>
+    <div>
+      {Header}
 
-        <Grid xs={8}>
-          {ChildGrid}
-          {ChildGrid}
+      <Box sx={{ flexGrow: 1 }} className={styles.container}>
+        <Grid container spacing={2}>
+          <Grid xs={4}>{Total}</Grid>
+
+          <Grid xs={8}>
+            {ChildGrid}
+            {ChildGrid}
+          </Grid>
         </Grid>
-      </Grid>
-    </Box>
+      </Box>
+    </div>
   )
 }
